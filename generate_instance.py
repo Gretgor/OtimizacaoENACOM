a9523850e43b59d38250bacc8c4515fa7fe77bce
Fabricio <gretgor42@gmail.com>
2022-08-05 17:31:25 -0300

    Updating instance generator

--- generate_instance.py
+++ generate_instance.py
@@ -1,5 +1,18 @@
 # GENERATE INSTANCES FOR THE INVESTMENT PROGRAM
 import sys
+import random
 
-for i in sys.argv:
-	print(i)
+# NUMBER OF INVESTMENT OPTIONS
+instance_size = int(sys.argv[1])
+
+# there will be at most n/2 conflicts in these test cases
+conflict_size = random.randint(0,int(instance_size/2))
+
+# there will be at most n/2 dependencies in these test cases
+dependency_size = random.randint(0,int(instance_size/2))
+
+# budget is set to a random number between 30 and 1000:
+budget = random.randint(30,1000)
+print(budget,dependency_size,conflict_size)
+
+
