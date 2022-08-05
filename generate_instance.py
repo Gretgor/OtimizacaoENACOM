6228e02a86bc4f83053499d56647c6b332d565f2
Fabricio <gretgor42@gmail.com>
2022-08-05 19:05:54 -0300

    Just adding some more funny names

--- generate_instance.py
+++ generate_instance.py
@@ -12,7 +12,7 @@ conflict_size = random.randint(0,int(instance_size/2))
 dependency_size = random.randint(0,int(instance_size/2))
 
 # budget is set to a random number between 30 and 1000*int(instance_size/8):
-budget = random.randint(30,1000*int(instance_size/8))
+budget = random.randint(30,1000*max(int(instance_size/8),1))
 print(instance_size)
 print(budget)
 
@@ -26,9 +26,9 @@ for i in range(0,instance_size):
 
 # name generation (just having fun with this one)
 for i in range(0,instance_size):
-	part1 = random.choice(["Atualização","Aprimoramento","Expansão","Capacitação","Compra","Criação","Reforma","Refatoração","Venda","Anúncio"])
-	part2 = random.choice([" de pessoal"," de armazém"," de portfolio"," de equipamento"," de máquina"," dos relatórios"," de funcionários"])
-	part3 = random.choice([" para atividade fim"," para limpeza"," em relações públicas"," em 10%"," de segurança"," de desenvolvimento"," de suporte"])
+	part1 = random.choice(["Atualização","Aprimoramento","Expansão","Capacitação","Compra","Criação","Reforma","Refatoração","Venda","Anúncio","Reajuste","Palestras sobre a importância"])
+	part2 = random.choice([" de pessoal"," de armazém"," de portfolio"," de equipamento"," de máquina"," dos relatórios"," de funcionários"," da remuneração"," da prevenção"])
+	part3 = random.choice([" para atividade fim"," para limpeza"," de funcionários"," em relações públicas"," em 10%"," de segurança"," de desenvolvimento"," de suporte"," de aposentadoria"," de acidentes"])
 	print(str(i+1)+": "+part1+part2+part3)
 	
 # conflicts and dependencies: for every one of those, just print two random indices
