6ce245dc955ea04f7ea8421355016e44d214c262
Fabricio <gretgor42@gmail.com>
2022-08-06 22:32:27 -0300

    There was a GRAVE mistake in the previous version of the experiment program. It is kind of hilarious, to be honest.

--- experiment.sh
+++ experiment.sh
@@ -11,7 +11,7 @@ do
 		echo "Generating..."
 		python3 generate_instance.py $i > TEMP_INST
 		echo "Executing..."
-		{ time python3.7 investment.py < penis ; } 2> EXECUTION 1> /dev/null
+		{ time python3.7 investment.py < TEMP_INST ; } 2> EXECUTION 1> /dev/null
 		# Getting "real" time, no reason to use the other two that I can think of
 		tempo=$(cat EXECUTION | grep real | cut -d"m" -f2 | cut -d"s" -f1 | sed s/,//g)
 		sum=$((sum+tempo))
